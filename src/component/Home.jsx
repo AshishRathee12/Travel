1732800ac91f9374e16f7eca175cd072e6090fc7
Aashish-Rathi <ashishrathi0002@gmail.com>
2025-02-16 21:39:39 +0530

    travel project

---
+++ src/component/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CollapsibleExample from './Navbar/CollapsibleExample'
+
+export default function Home() {
+    return (
+        <>
+            <CollapsibleExample />
+        </>
+    )
+}
